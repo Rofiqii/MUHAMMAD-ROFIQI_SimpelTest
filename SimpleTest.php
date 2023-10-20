85ddbaf5d0f066df09cd98593c4029f6aaf02480
salmongurun <rofiqimuhammad437@gmail.com>
2023-10-20 09:50:12 +0700

    tugas

--- SimpleTest.php
+++ SimpleTest.php
@@ -13,4 +13,11 @@ class SimpleTest extends TestCase
         $this->assertEquals(4, $WordCount);
     }
 }
+// Baris 1-2: Mendefinisikan namespace dan mengimport kelas TestCase dari framework PHPUnit.
+// Baris 4: Mengimport kelas WordCount dari file Wordcount.php.
+// Baris 6-12: Mendefinisikan kelas SimpleTest yang merupakan turunan dari kelas TestCase. Kelas ini berisi sebuah metode bernama testcountWords() yang akan menguji fungsi countWords().
+// Baris 8: Membuat objek $Wc dari kelas WordCount.
+// Baris 9: Membuat variabel $TestSentence yang berisi kalimat yang akan diuji.
+// Baris 10: Memanggil fungsi countWords() dari objek $Wc dengan parameter $TestSentence, dan menyimpan hasilnya pada variabel $WordCount.
+// Baris 11: Membandingkan nilai variabel $WordCount dengan nilai 4 menggunakan fungsi
 ?>
--- Wordcount.php
+++ Wordcount.php
@@ -4,4 +4,8 @@ class Wordcount{
         return count(explode(" ", $sentence));
     }
 }
+// Baris 1-5: Mendefinisikan kelas Wordcount dengan menggunakan kata kunci class. Kelas ini memiliki satu metode bernama countWords().
+// Baris 2-4: Mendefinisikan metode countWords(). Metode ini menerima satu parameter bernama $sentence, yang merupakan kalimat yang akan dihitung jumlah katanya.
+// Baris 3: Menggunakan fungsi explode() untuk memecah kalimat menjadi array kata-kata, dengan pemisah spasi.
+// Baris 4: Menggunakan fungsi count() untuk menghitung jumlah elemen pada array kata-kata, yang merupakan jumlah kata pada kalimat.
 ?>
